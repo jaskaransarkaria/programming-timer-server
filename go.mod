c3c03f07a0bbad464e08a28fbf0fe93405a8f7b3
jaskaran <jaskaran.sarkaria@googlemail.com>
2020-04-03 16:32:27 +0100

    fix: 🐛 attempt to unmarshal json

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jaskaransarkaria/programming-timer-server
 
 go 1.14
 
-require github.com/gorilla/websocket v1.4.2
+require (
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/websocket v1.4.2
+)
--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 	"net/http"
 	"log"
 	"flag"
 	"github.com/gorilla/websocket"
+	"github.com/google/uuid"
 )
+
+type StartTimer struct {
+	uuid string `json:"uuid"`
+	Duration string `json:"duration"`
+	StartTime string `json:"startTime"`
+}
 
 // flag allows you to create cli flags and assign a default
 var addr = flag.String("addr", "localhost:8080", "http service address")
@@ -34,16 +42,32 @@ func writer(conn *websocket.Conn, messageType int, message []byte) {
 func reader(conn *websocket.Conn) {
 	// listen on this connection for new messages and send messages down that connection
 	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		log.Println(string(p))
+			messageType, p, err := conn.ReadMessage()
+			log.Println(string(p))
+			if err != nil {
+				log.Println(err)
+				return
+			}
 
-		//message back to the client
-		writer(conn, messageType, []byte("This is a response through the websocket connection from the server"))
-	}
+			var startTimerData StartTimer
+			err = json.Unmarshal(p, &startTimerData)
+			
+			if err != nil {
+				id, err := uuid.NewUUID()
+				if err != nil {
+					log.Println("error from new uuid")
+					return
+				}
+				log.Println(id.String())
+				writer(conn, messageType, []byte("well done you've connected via web sockets to a go server"))
+				writer(conn, messageType, []byte(id.String()))
+				log.Println(startTimerData)		
+				fmt.Println("error:", err)
+				return
+			}
+		log.Println(startTimerData)		
+		return
+		}
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
@@ -57,6 +81,7 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Println("Client successfully connected to Golang Websocket!")
+	// either read json or read message
 	reader(ws)
 }
 
@@ -71,4 +96,10 @@ func main() {
 	flag.Parse()
 	fmt.Println("Listening on:", *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
+	// var v interface{}
+	// 	json.Unmarshal(p, &v)
+	// 	data := v.(map[string]interface{})
+
+	// 	log.Println(data)
+
 }
